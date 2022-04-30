48160a403822ee6d29345802a17b067ccee64bf5
omkarajagunde <omkarajagunde.biencaps@gmail.com>
2022-04-30 23:10:33 +0530

    changes

--- reducers/commonReducer.js
+++ reducers/commonReducer.js
@@ -1,18 +1,10 @@
-import { DEMO_SUCCESS, DEMO_FAILURE } from "../actions/types";
+import {} from "../actions/types";
 
 const INIT_STATE = {};
 
 const LiveChatReducer = (state = INIT_STATE, action) => {
 	const { type, payload } = action;
 	switch (type) {
-		case DEMO_SUCCESS:
-			return {
-				...state,
-			};
-
-		case DEMO_FAILURE:
-			return {};
-
 		default:
 			return state;
 	}
