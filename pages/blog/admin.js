19a48d9bd2a68afd417bbe8b067887d9d4373879
omkarajagunde <omkarajagunde.biencaps@gmail.com>
2022-05-03 18:21:59 +0530

    changes

---
+++ pages/blog/admin.js
@@ -0,0 +1,267 @@
+import React, { useRef, useState } from "react";
+import Footer from "../../components/Footer";
+import NavBar from "../../components/NavBar";
+import dynamic from "next/dynamic";
+import axios from "axios";
+import Compressor from "compressorjs";
+import { Tabs, Tab, Button, Form, Alert } from "react-bootstrap";
+import ViewBlogs from "../../components/ViewBlogs";
+import admins from "../../Resources/blog-admin-list";
+import "suneditor/dist/css/suneditor.min.css";
+// Styles!
+import styles from "../../styles/Admin.module.scss";
+const SunEditor = dynamic(() => import("suneditor-react"), {
+	//besure to import dynamically
+	ssr: false,
+});
+
+const plugins = [
+	[
+		"undo",
+		"redo",
+		"fontSize",
+		"formatBlock",
+		"paragraphStyle",
+		"blockquote",
+		"bold",
+		"underline",
+		"italic",
+		"strike",
+		"subscript",
+		"superscript",
+		"fontColor",
+		"hiliteColor",
+		"textStyle",
+		"removeFormat",
+		"outdent",
+		"indent",
+		"align",
+		"horizontalRule",
+		"list",
+		"lineHeight",
+		"table",
+		"link",
+		"image",
+		"video",
+		"fullScreen",
+		"showBlocks",
+		"codeView",
+		"preview",
+		/** 'dir', 'dir_ltr', 'dir_rtl' */ // "dir": Toggle text direction, "dir_ltr": Right to Left, "dir_rtl": Left to Right
+	],
+];
+
+function admin() {
+	const editor = useRef();
+	const [show, setShow] = useState(true);
+	const [state, setState] = useState({
+		editorContent: "",
+		blogTitle: "",
+		blogKeywords: "",
+		blogShortDesc: "",
+		blogImageFile: null,
+		loggedIn: true,
+		logInError: null,
+		newBlogAdded: false,
+	});
+
+	// The sunEditor parameter will be set to the core suneditor instance when this function is called
+	const getSunEditorInstance = (sunEditor) => {
+		editor.current = sunEditor;
+	};
+
+	const handleSave = () => {
+		let keywordsArr = state.blogKeywords.split(",");
+		keywordsArr.forEach((kwd) => kwd.trim());
+		console.log(state);
+		let img = document.getElementById("captionImage");
+
+		let body = {
+			blogTitle: state.blogTitle,
+			metaKeywords: keywordsArr,
+			shortDesc: state.blogShortDesc,
+			blogHtml: state.editorContent,
+			blogImage: "",
+		};
+
+		new Compressor(state.blogImageFile, {
+			quality: 0.8,
+
+			// The compression process is asynchronous,
+			// which means you have to access the `result` in the `success` hook function.
+			success(result) {
+				let time = new Date();
+				var reader = new FileReader();
+				reader.readAsDataURL(result);
+				reader.onload = (e) => {
+					body.blogImage = e.target.result;
+					axios
+						.post(`${process.env.NEXT_PUBLIC_BLABLAH_URL}/api/blog`, body)
+						.then((response) => {
+							// blog saved successfully
+							console.log(response);
+							setState((prevState) => ({ ...prevState, newBlogAdded: true }));
+							setTimeout(() => {
+								window.location.reload();
+							}, 1000);
+						})
+						.catch((err) => {
+							// Error saving blog...handle here
+						});
+				};
+			},
+			error(err) {
+				console.log(err.message);
+			},
+		});
+	};
+
+	const handleBlogBlur = (event, content) => {
+		setState((prevState) => ({ ...prevState, editorContent: content }));
+	};
+
+	const handleTitleBlur = (event) => {
+		setState((prevState) => ({ ...prevState, blogTitle: event.target.value }));
+	};
+
+	const handleMetaKeywordsBlur = (event) => {
+		setState((prevState) => ({ ...prevState, blogKeywords: event.target.value }));
+	};
+
+	const handleShortDescBlur = (event) => {
+		setState((prevState) => ({ ...prevState, blogShortDesc: event.target.value }));
+	};
+
+	const handleFileUploadChange = (event) => {
+		setState((prevState) => ({ ...prevState, blogImageFile: event.target.files[0] }));
+	};
+
+	const handleOnClear = () => {
+		setState((prevState) => ({ ...prevState, blogImageFile: null, blogShortDesc: "", blogKeywords: "", blogTitle: "", editorContent: "" }));
+		editor.current.setContents("");
+	};
+
+	const handleLogin = () => {
+		setState((prevState) => ({ ...prevState, loggedIn: false, logInError: null }));
+		setShow(false);
+		let email = document.getElementById("email");
+		let password = document.getElementById("password");
+		if (email.value.length > 4 && password.value.length > 4) {
+			// make a api call to login the user ...and set loggenIn user as true in state
+			admins.forEach((admin) => {
+				if (admin.email === email.value.trim() && admin.password === password.value.trim()) {
+					setState((prevState) => ({ ...prevState, loggedIn: true, logInError: null }));
+				} else {
+					// Wrong username password
+					setState((prevState) => ({ ...prevState, loggedIn: false, logInError: "Wrong email and/or password provided" }));
+					setShow(true);
+				}
+			});
+		} else {
+			// Wrong username password
+			setState((prevState) => ({ ...prevState, loggedIn: false, logInError: "Enter valid email/password, both should be ateast 4 letters" }));
+			setShow(true);
+		}
+	};
+
+	if (!state.loggedIn) {
+		return (
+			<>
+				<NavBar />
+
+				<div className={styles.adminContainer} style={{ marginBottom: "30px", maxWidth: "400px", padding: "10px", height: "500px" }}>
+					{state.logInError && show && (
+						<Alert variant="danger" onClose={() => setShow(false)} dismissible>
+							<Alert.Heading>Oh error!</Alert.Heading>
+							<p>{state.logInError}</p>
+						</Alert>
+					)}
+					<div className={styles.adminContainer__title}>Manage Blog</div>
+
+					<Form.Group className="mb-3">
+						<Form.Label>Email address</Form.Label>
+						<Form.Control id="email" type="email" placeholder="Enter email" />
+						<Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+					</Form.Group>
+
+					<Form.Group className="mb-3">
+						<Form.Label>Password</Form.Label>
+						<Form.Control id="password" type="password" placeholder="Password" />
+					</Form.Group>
+					<Button variant="primary" type="submit" onClick={handleLogin}>
+						Login
+					</Button>
+				</div>
+			</>
+		);
+	}
+
+	if (state.loggedIn) {
+		return (
+			<>
+				<NavBar />
+				<div className={styles.adminContainer}>
+					<div className={styles.adminContainer__title}>Manage Blog</div>
+					<Tabs defaultActiveKey="newBlog" id="uncontrolled-tab-example" className="mb-3">
+						<Tab eventKey="newBlog" title="New Blog">
+							<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+								<Form.Label>Enter Blog Title</Form.Label>
+								<Form.Control maxLength={300} onBlur={handleTitleBlur} placeholder="e.g. This is a catchy blog title which would be 20 -40 words long" />
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+								<Form.Label>Enter Meta keywords (comma seperated ",")</Form.Label>
+								<Form.Control maxLength={800} onBlur={handleMetaKeywordsBlur} placeholder="e.g. programming, science, digital marketing" />
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+								<Form.Label>Enter short description</Form.Label>
+								<Form.Control
+									onBlur={handleShortDescBlur}
+									placeholder="e.g. Enter a freeform short 100 - 120 words description that would be shown as a caption of blog"
+									as="textarea"
+									rows={4}
+									maxLength={800}
+								/>
+							</Form.Group>
+
+							<Form.Group>
+								<Form.Label>Image selector</Form.Label>
+								<Form.Control onChange={handleFileUploadChange} type="file" accept="image/*" id="captionImage" />
+							</Form.Group>
+							<div className={styles.adminContainer__newBlogEditor} id="editor">
+								<SunEditor
+									width="100%"
+									value={state.editorContent}
+									onBlur={handleBlogBlur}
+									getSunEditorInstance={getSunEditorInstance}
+									setOptions={{ height: "300px", buttonList: plugins }}
+									setDefaultStyle="font-family:'Montserrat', sans-serif"
+								/>
+							</div>
+							<div className={styles.adminContainer__newBlogControls}>
+								<Button variant="primary" onClick={handleOnClear}>
+									Clear All
+								</Button>
+								<Button variant="primary" onClick={handleSave}>
+									Save Blog
+								</Button>
+							</div>
+							<div className={styles.adminContainer__newBlogAddedAlert}>
+								{state.newBlogAdded && (
+									<Alert variant="success" onClose={() => setShow(false)} dismissible>
+										<Alert.Heading>New Blog added successfully, refreshing window</Alert.Heading>
+									</Alert>
+								)}
+							</div>
+						</Tab>
+						<Tab eventKey="viewBlog" title="View Blogs">
+							<ViewBlogs />
+						</Tab>
+					</Tabs>
+				</div>
+				<Footer />
+			</>
+		);
+	}
+}
+
+export default admin;
---
+++ pages/blog/index.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect, useRef } from "react";
+import AwesomeDebouncePromise from "awesome-debounce-promise";
+import Footer from "../../components/Footer";
+import NavBar from "../../components/NavBar";
+import Image from "next/image";
+import Link from "next/link";
+import axios from "axios";
+import ReadingProgress from "../../components/ReadingProgress";
+
+// Styles!
+import styles from "../../styles/Blog.module.scss";
+
+// Icons!
+import TitleIcon from "../../Resources/the-blog-graphics.svg";
+
+function index(props) {
+	const target = useRef(null);
+	const [state, setState] = useState({
+		isMobileView: false,
+		blogCardsArr: props.blogTopics,
+	});
+
+	// Run only first time when component loads
+	useEffect(() => {
+		// set initial level
+		if (window.innerWidth <= 768) setState((prevState) => ({ ...prevState, isMobileView: true }));
+		else setState((prevState) => ({ ...prevState, isMobileView: false }));
+
+		console.log("BlogTopics ;: ", props.blogTopics);
+
+		window.addEventListener("resize", () => {
+			if (window.innerWidth <= 768) setIsMobileViewDebouncer(true);
+			else setIsMobileViewDebouncer(false);
+		});
+
+		return () => {
+			window.removeEventListener("resize", () => {});
+		};
+	}, []);
+
+	// Debouncing resize event
+	const setIsMobileViewDebouncer = AwesomeDebouncePromise((flag) => {
+		setState((prevState) => ({ ...prevState, isMobileView: flag }));
+		console.log("resize event triggered, updating local component state");
+	}, 500);
+
+	const getTimeToRead = (text) => {
+		return "8 mins";
+	};
+
+	return (
+		<div>
+			<ReadingProgress target={target} />
+			<NavBar />
+			<div className={styles.blogHome} ref={target}>
+				<div className={styles.blogHome__title}>The Blog!</div>
+				<div className={styles.blogHome__subtitle}>
+					<div className={styles.blogHome__subtitleContainer}>
+						<div>This is the blog, hope you find it very useful</div>
+						<div>
+							<Image src={TitleIcon.src} width={state.isMobileView ? 75 : 150} height={state.isMobileView ? 75 : 150} alt={`the-blog-icon`} />
+						</div>
+					</div>
+				</div>
+				<div className={styles.blogHome__fullView}>
+					{state.blogCardsArr.map((blog) => (
+						<div className={styles.blogHome__viewCard}>
+							<Link
+								href={{
+									pathname: "/blog/" + blog.blogSlug,
+								}}
+							>
+								<div className={styles.blogCard}>
+									<div className={styles.blogCard__image}>
+										<img src={blog.blogImage} alt={blog.blogImageAlt || ""} />
+									</div>
+									<div className={styles.blogCard__text}>
+										<div className={styles.blogCard__title} style={{ maxWidth: "100%", fontSize: "1rem" }}>
+											{blog.blogTitle}
+										</div>
+										<div className={styles.blogCard__timestamp} style={{ maxWidth: "100%", fontSize: ".7rem" }}>
+											<div>{blog.publishedAt}</div>
+											<div>{getTimeToRead(blog.blogHtml)}</div>
+										</div>
+										<div className={styles.blogCard__subtitle} style={{ maxWidth: "100%", fontSize: ".8rem", WebkitLineClamp: state.isMobileView ? 3 : 5 }}>
+											{blog.shortDesc}
+										</div>
+									</div>
+								</div>
+							</Link>
+						</div>
+					))}
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+	let response = await axios.get(`${process.env.NEXT_PUBLIC_BLABLAH_URL}/api/blog?topics=true`);
+	const blogTopics = response.data.data;
+	// Pass ] data to the page via props
+	return { props: { blogTopics: [...blogTopics] }, revalidate: 1 };
+}
+
+export default index;
