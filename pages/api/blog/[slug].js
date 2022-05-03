35722f9fa52cecebeafb53004803134747887ec7
omkarajagunde <omkarajagunde.biencaps@gmail.com>
2022-05-03 23:24:34 +0530

    change

--- pages/api/blog/[slug].js
+++ pages/api/blog/[slug].js
@@ -4,13 +4,13 @@ import NextCors from "nextjs-cors";
 
 export default async function handler(req, res) {
 	// Run the cors middleware
-	// nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
-	await NextCors(req, res, {
-		// Options
-		methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-		origin: ["blablah.app/", "blablah.app/"],
-		optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-	});
+	// // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+	// await NextCors(req, res, {
+	// 	// Options
+	// 	methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+	// 	origin: ["blablah.app/", "blablah.app/"],
+	// 	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+	// });
 	const { method, body, query } = req;
 	const { slug } = query;
 	await dbConnect();
