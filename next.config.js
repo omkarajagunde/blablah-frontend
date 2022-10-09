e53ebe1bda731039f4dc2f542e4467199b50c15c
Omkar-Ajagunde <omkar.ajagunde@ibm.com>
2022-10-09 20:26:25 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -7,12 +7,25 @@ module.exports = {
 	async headers() {
 		return [
 			{
-				source: "/:all*(svg|jpg|png)",
-				locale: false,
+				// This works, and returns appropriate Response headers:
+				source: "/(.*).jpg",
 				headers: [
 					{
 						key: "Cache-Control",
-						value: "public, max-age=9999999999, must-revalidate"
+						value: "public, max-age=9999999, s-maxage=9999999, stale-while-revalidate=9999999"
+					}
+				]
+			},
+			{
+				// This doesn't work for 'Cache-Control' key (works for others though):
+				source: "/_next/image(.*)",
+				headers: [
+					{
+						key: "Cache-Control",
+						// Instead of this value:
+						value: "public, max-age=9999999, s-maxage=9999999, stale-while-revalidate=9999999"
+						// Cache-Control response header is `public, max-age=60` in production
+						// and `public, max-age=0, must-revalidate` in development
 					}
 				]
 			}
