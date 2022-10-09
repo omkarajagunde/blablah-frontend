109494d32bddcc226e40319f136c6ddf466a1c1a
Omkar-Ajagunde <omkar.ajagunde@ibm.com>
2022-10-09 20:23:51 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -3,5 +3,19 @@ module.exports = {
 	images: {
 		domains: ["https://i.pinimg.com"],
 		minimumCacheTTL: 23600
+	},
+	async headers() {
+		return [
+			{
+				source: "/:all*(svg|jpg|png)",
+				locale: false,
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=9999999999, must-revalidate"
+					}
+				]
+			}
+		];
 	}
 };
