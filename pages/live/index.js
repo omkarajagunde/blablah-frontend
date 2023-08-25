8b73b4f5e5e94ef9f5fe5c3b880d09bd8bcee3f6
Omkar-Ajagunde <omkar.ajagunde@ibm.com>
2023-08-25 19:47:31 +0530

    changes

--- pages/live/index.js
+++ pages/live/index.js
@@ -1253,13 +1253,13 @@ function Index() {
 	const { isMobileView } = state;
 	return (
 		<div style={{ height: "100%" }}>
-			<Script type="text/javascript">
+			{/* <Script type="text/javascript">
 				{`(function(c,l,a,r,i,t,y){
                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
                 })(window, document, "clarity", "script", "ikp0t7an2x");`}
-			</Script>
+			</Script> */}
 
 			<Head>
 				<meta name="theme-color" content="#474663" />
