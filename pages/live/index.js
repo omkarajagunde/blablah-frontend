0c054a58fd201d39ac2676dbec8a27cb06fed693
Omkar-Ajagunde <omkar.ajagunde@ibm.com>
2023-08-25 15:23:46 +0530

    added microsoft clarity integration

--- pages/live/index.js
+++ pages/live/index.js
@@ -1254,6 +1254,13 @@ function Index() {
 	const { isMobileView } = state;
 	return (
 		<div style={{ height: "100%" }}>
+			<Script type="text/javascript">
+				{`(function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "ikp0t7an2x");`}
+			</Script>
 			<Script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" defer />
 			<Script id="google-analytics" strategy="afterInteractive">
 				{`
