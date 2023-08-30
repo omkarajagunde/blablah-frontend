08a63bfabffd895b2c40d18f6df814f2b9b54be9
Omkar-Ajagunde <omkar.ajagunde@ibm.com>
2023-08-30 14:39:31 +0530

    GA intergrated

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,21 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
 			<Analytics />
+			{/* <Script type="text/javascript">
+				{`(function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "ikp0t7an2x");`}
+			</Script> */}
+			<Script src="https://www.googletagmanager.com/gtag/js?id=G-97F16HF9GJ" />
+			<Script type="text/javascript">
+				{` window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-97F16HF9GJ');`}
+			</Script>
 			<HighlightInit
 				projectId={"kev2k3g3"}
 				tracingOrigins
