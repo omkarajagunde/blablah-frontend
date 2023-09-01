989d499049fd9a9d13cbf41a5be093f4867a02b4
Omkar-Ajagunde <omkar.ajagunde@ibm.com>
2023-09-01 12:30:04 +0530

    changes

--- pages/ads.js
+++ pages/ads.js
@@ -4,17 +4,14 @@ import React from "react";
 function Ads() {
 	return (
 		<div>
-			<Script src="https://tally.so/widgets/embed.js" strategy="beforeInteractive" />
-			<iframe
-				style={{ width: "100vw", height: "100vh" }}
-				data-tally-src="https://tally.so/r/nraqVL?transparentBackground=1"
-				width="100%"
-				height="100%"
-				frameborder="0"
-				marginheight="0"
-				marginwidth="0"
-				title="Blablah Ads"
-			></iframe>
+			<iframe style={{ width: "100vw", height: "100vh" }} data-tally-src="https://tally.so/r/nraqVL?transparentBackground=1" title="Blablah Ads"></iframe>
+			<Script
+				id="tally-js"
+				src="https://tally.so/widgets/embed.js"
+				onLoad={() => {
+					Tally.loadEmbeds();
+				}}
+			/>
 		</div>
 	);
 }
